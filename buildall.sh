6f469dd5479d75dddf6329a6f43571accd5506d1
Tim Keller <turbofx@gmail.com>
2024-11-23 09:10:07 -0500

    simple script that generates the pdf/txt/epub versions

---
+++ buildall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+SOFFICE=$(which soffice)
+
+echo "Building PDF"
+$SOFFICE --convert-to pdf aliens-return-to-lv426.odt aliens-return-to-lv426.pdf
+echo "Building TXT"
+$SOFFICE --convert-to txt aliens-return-to-lv426.odt aliens-return-to-lv426.txt
+echo "Building EPUB"
+$SOFFICE --convert-to epub aliens-return-to-lv426.odt aliens-return-to-lv426.epub
+
+
