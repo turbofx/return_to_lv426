69b702bcc43d561012f6c6f05d2314dafdc63731
Tim Keller <turbofx@gmail.com>
2024-12-28 10:50:06 -0500

    grammar clean up, more plot details.

--- buildall.sh
+++ buildall.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/bash
 SOFFICE=$(which soffice)
 
-echo "Building PDF"
-$SOFFICE --convert-to pdf aliens-return-to-lv426.odt aliens-return-to-lv426.pdf
-echo "Building TXT"
-$SOFFICE --convert-to txt aliens-return-to-lv426.odt aliens-return-to-lv426.txt
-echo "Building EPUB"
-$SOFFICE --convert-to epub aliens-return-to-lv426.odt aliens-return-to-lv426.epub
-echo "Building HTML"
-$SOFFICE --convert-to html aliens-return-to-lv426.odt aliens-return-to-lv426.html
+for i in pdf txt epub html
+do
+    echo "Building $i"
+    $SOFFICE --convert-to $i aliens-return-to-lv426.odt aliens-return-to-lv426.$i
+done
+echo "Done."
 
-
+git add aliens-return-to-lv426.*
